10256a1d4b734aa0d05f503f03b34a79dc1c3829
Simon Oulevay <simon.oulevay@gmail.com>
2021-09-23 14:44:32 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Angular PDF Experiments
+
+[![License](https://img.shields.io/static/v1?label=license&message=MIT&color=informational)](./LICENSE.txt)
+
+Experimenting with generating a PDF from an Angular application in the browser.
+
+## Usage
+
+```bash
+git clone https://github.com/AlphaHydrae/angular-pdf-experiments.git
+cd angular-pdf-experiments
+npm ci
+npm start
+```
+
+Your browser should open at http://localhost:4200.
---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import * as html2pdf from 'html2pdf.js';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Angular PDF Experiments';
+
+  @ViewChild("main", { static: true })
+  private readonly mainElement!: ElementRef<HTMLElement>;
+
+  async saveAsPdf() {
+    html2pdf().from(this.mainElement.nativeElement).save();
+  }
+}
